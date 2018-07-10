a0b3034d14c07f09c34be4ec4769af5dd2a76de7
Lisakovich Vadim <midav.lis@gmail.com>
2018-07-10 09:30:57 +0300

    moved test variables

--- src/core/demo/ts/demo/TemplateManager.ts
+++ src/core/demo/ts/demo/TemplateManager.ts
@@ -1,42 +1,42 @@
 import { EditorKindDoc } from "tinymce/core/KindDoc"
 
+const testPrj = [
+    {
+        "title": "Номер РК",
+        "url": "../tmpl/DOC_RC/freeNum.htm",
+        "description": "Значение номера документа"
+    },
+    {
+        "title": "Адресаты с датой отправки",
+        "url": "../tmpl/DOC_RC/addresses.htm",
+        "description": "Перечень адресатов с датой отправки"
+    },
+    {
+        "title": "Дата РК",
+        "url": "../tmpl/DOC_RC/docDate.htm",
+        "description": "Дата РК"
+    }
+];
+
+const testDoc = [
+    {
+        "title": "For Doc",
+        "url": "../tmpl/DOC_RC/freeNum.htm",
+        "description": "Значение номера документа"
+    },
+    {
+        "title": "Адресаты с датой отправки",
+        "url": "../tmpl/DOC_RC/addresses.htm",
+        "description": "Перечень адресатов с датой отправки"
+    },
+    {
+        "title": "Дата РК",
+        "url": "../tmpl/DOC_RC/docDate.htm",
+        "description": "Дата РК"
+    }
+];
+
 const getTemplates = function (editor, showDialogCallback) {
-    let testPrj = [
-        {
-            "title": "Номер РК",
-            "url": "../tmpl/DOC_RC/freeNum.htm",
-            "description": "Значение номера документа"
-        },
-        {
-            "title": "Адресаты с датой отправки",
-            "url": "../tmpl/DOC_RC/addresses.htm",
-            "description": "Перечень адресатов с датой отправки"
-        },
-        {
-            "title": "Дата РК",
-            "url": "../tmpl/DOC_RC/docDate.htm",
-            "description": "Дата РК"
-        }
-    ];
-
-    let testDoc = [
-        {
-            "title": "For Doc",
-            "url": "../tmpl/DOC_RC/freeNum.htm",
-            "description": "Значение номера документа"
-        },
-        {
-            "title": "Адресаты с датой отправки",
-            "url": "../tmpl/DOC_RC/addresses.htm",
-            "description": "Перечень адресатов с датой отправки"
-        },
-        {
-            "title": "Дата РК",
-            "url": "../tmpl/DOC_RC/docDate.htm",
-            "description": "Дата РК"
-        }
-    ];
-
     let kindDoc = editor.getKindDoc();
 
     switch (kindDoc) {
